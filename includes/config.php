6d073ce83295fa959d504185c6abea84d63e2eaf
second_coming <secondcoming@users.noreply.github.com>
2015-10-11 12:05:12 +0100

    Update config.php
    added mysql port to the config

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,4 @@
 <?php
-// array of servers ('hostname or ip address|database name|database username|database password|server name')
-$ServerList = array('127.0.0.1|exile_altis|dbuser|dbpass|altis','127.0.0.1|exile_chernarus|dbuser|dbpass|chernarus'); 
+// array of servers ('hostname or ip address|database name|database username|database password|server name|mysql port')
+$ServerList = array('127.0.0.1|exile_altis|dbuser|dbpass|altis|3306','127.0.0.1|exile_chernarus|dbuser|dbpass|chernarus|3306'); 
 ?>
