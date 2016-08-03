7b6f2ae1d2e80fe5223a3639c172f67544d4c807
second_coming <secondcoming@users.noreply.github.com>
2016-08-03 20:28:32 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,4 @@
-<?php// array of servers ['host|dbname|db user|db pass|name']$ServerList = array('127.0.0.1|exile0940|dbuser|dbuser|Tanoa|3306'); ?>
+<?php
+// array of servers ['host|dbname|db user|db pass|name']
+$ServerList = array('127.0.0.1|exile|dbusername|dbpassword|Tanoa|3306'); 
+?>
