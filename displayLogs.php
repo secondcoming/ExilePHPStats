eb97f355e6deec9d98217877a2714a870de716c7
second_coming <secondcoming@users.noreply.github.com>
2016-08-30 16:13:52 +0100

    Update displayLogs.php

--- displayLogs.php
+++ displayLogs.php
@@ -6,35 +6,51 @@ include 'includes/header.php';
 include 'includes/config.php';
 include 'includes/functions.php';
 
-$sql = "SELECT * FROM infistar_logs ORDER BY time DESC LIMIT 1000";
-$result = mysqli_query($db_local, $sql);
-
-if (mysqli_num_rows($result) > 0)
+foreach ($ServerList as $ServerToCheck)
 {
-    echo "<hr><h2>infiSTAR Logs</h2><hr>";
-    echo '<table class="tftable" border="1"">
-            <tr>
-                <td style="width:150px;">logname</td>
-                <td style="width:450px;">logentry</td>
-                <td style="width:80px;">time</td>            
-            </tr>';
-}
-else
-{
-    echo "<hr><h2>There is no log history</h2><hr>";
+	$ServerDetails = explode("|", $ServerToCheck);
+	$dbhost = $ServerDetails[0];
+	$dbname = $ServerDetails[1];
+	$dbuser = $ServerDetails[2];
+	$dbpass = $ServerDetails[3];
+	$servername = ucwords($ServerDetails[4]);
+	$dbport = $ServerDetails[5];
+	$db_local = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+
+	$sql = "SELECT * FROM infistar_logs ORDER BY time DESC LIMIT 1000";
+	$result = mysqli_query($db_local, $sql);
+
+	if (mysqli_num_rows($result) > 0)
+	{
+		echo "<hr><h2>infiSTAR Logs on the $servername server</h2><hr>";
+		echo '<table class="tftable" border="1"">
+				<tr>
+					<td style="width:150px;">logname</td>
+					<td style="width:450px;">logentry</td>
+					<td style="width:80px;">time</td>            
+				</tr>';
+	}
+	else
+	{
+		echo "<hr><h2>There is no log history</h2><hr>";
+	}
+
+	while ($row = mysqli_fetch_object($result))
+	{
+		$logname    = $row->logname;
+		$logentry   = $row->logentry;
+		$time       = $row->time;
+
+		echo '<tr>'
+		. '<td valign=top>' . $logname . '</td>'
+		. '<td valign=top>' . $logentry . '</td>'
+		. '<td valign=top>' . $time . '</td>'
+		. '</tr>';
+	}
+	echo "</table>";	
+	
 }
 
-while ($row = mysqli_fetch_object($result))
-{
-    $logname    = $row->logname;
-    $logentry   = $row->logentry;
-    $time       = $row->time;
 
-    echo '<tr>'
-    . '<td valign=top>' . $logname . '</td>'
-    . '<td valign=top>' . $logentry . '</td>'
-    . '<td valign=top>' . $time . '</td>'
-    . '</tr>';
-}
-echo "</table>";
+
 ?>
